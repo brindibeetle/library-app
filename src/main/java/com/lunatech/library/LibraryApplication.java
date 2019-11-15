c51d19f9646f7fc9344c2bcdd7b55b49bf4d1965
Brindi Beetle <brindi.beetle@gmail.com>
2019-11-15 15:18:51 +0100

    Added Unit tests

--- src/main/java/com/lunatech/library/LibraryApplication.java
+++ src/main/java/com/lunatech/library/LibraryApplication.java
@@ -2,10 +2,10 @@ package com.lunatech.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableResourceServer
 public class LibraryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lunatech/library/config/SecurityConfiguration.java
+++ src/main/java/com/lunatech/library/config/SecurityConfiguration.java
@@ -6,16 +6,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@Configuration
-@EnableOAuth2Sso
+//@Configuration
+//@EnableResourceServer
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+/*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
 //        http.oauth2Login();
 
-    }
+    }*/
 }
