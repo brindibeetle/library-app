f633d829960550ac8dd02e3945b7e330274ac015
Brindi Beetle <brindi.beetle@gmail.com>
2019-11-25 14:36:42 +0100

    Only lunatech domains are allowed (lunatech.nl, lunatech.be, lunatech.fr)

--- src/main/java/com/lunatech/library/config/CustumPrincipalExtractor.java
+++ src/main/java/com/lunatech/library/config/CustumPrincipalExtractor.java
@@ -2,11 +2,13 @@ package com.lunatech.library.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 
+import javax.naming.AuthenticationException;
 import java.util.Map;
 
 public class CustumPrincipalExtractor implements PrincipalExtractor {
     @Override
     public Object extractPrincipal(Map<String, Object> map) {
+
         return map.get("email");
     }
 }
--- src/main/java/com/lunatech/library/config/WebSecurityConfig.java
+++ src/main/java/com/lunatech/library/config/WebSecurityConfig.java
@@ -1,10 +1,19 @@
 package com.lunatech.library.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -18,4 +27,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Bean
+    public AuthoritiesExtractor authoritiesExtractor(
+            @Value("#{'${security.allowed.domains}'.split(',')}") final List<String> allowedDomains) {
+
+         return new AuthoritiesExtractor() {
+            @Override
+            public List<GrantedAuthority> extractAuthorities(final Map<String, Object> map) {
+                String email = (String)map.get("email");
+                String[] emailparts = email.split("@");
+                String emaildomain = emailparts[emailparts.length -1];
+                if (!allowedDomains.contains(emaildomain)) {
+                    throw new BadCredentialsException("Not an allowed domain");
+                }
+                return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+            }
+        };
+    }
 }
