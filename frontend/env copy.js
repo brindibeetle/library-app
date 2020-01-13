90b7d7323737ae2747e178b98c796872af87e28f
Brindi Beetle <brindi.beetle@gmail.com>
2020-01-13 10:55:58 +0100

    No caching of env.js by the use of a timestamp as parameter

---
+++ frontend/env copy.js
@@ -0,0 +1,7 @@
+var flags='\
+    {\
+        "google_oauth2_client_id" :   "937704847273-2ctk7g4e2qshu89gqch4at5qskqdus8n.apps.googleusercontent.com"\
+        , "library_api_base_url"   :   "http://localhost:8080/api/v1"\
+        ,"this_base_url"          :   "http://localhost:8000"\
+    }'
+
